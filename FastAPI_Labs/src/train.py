e43b259b5426c5981ea364e6148b511046392687
Zankhana Mehta <zankhanamehta@Zankhanas-MacBook-Air.local>
2025-09-20 12:56:06 -0400

    Initial commit with Lab 1 (FastAPI)

---
+++ FastAPI_Labs/src/train.py
@@ -0,0 +1,19 @@
+from sklearn.ensemble import RandomForestClassifier
+import joblib
+from data import load_data, split_data
+
+def fit_model(X_train, y_train):
+    """
+    Train a Decision Tree Classifier and save the model to a file.
+    Args:
+        X_train (numpy.ndarray): Training features.
+        y_train (numpy.ndarray): Training target values.
+    """
+    rf_classifier = RandomForestClassifier(max_depth=3, random_state=12)
+    rf_classifier.fit(X_train, y_train)
+    joblib.dump(rf_classifier, "../model/iris_model.pkl")
+
+if __name__ == "__main__":
+    X, y = load_data()
+    X_train, X_test, y_train, y_test = split_data(X, y)
+    fit_model(X_train, y_train)
---
+++ README.md
@@ -0,0 +1,7 @@
+# MLOps Labs
+
+This repository contains all my labs for the MLOps course.
+
+## Labs
+
+- [Lab 1: FastAPI + ML Model Deployment](Lab1_FastAPI/README.md)
